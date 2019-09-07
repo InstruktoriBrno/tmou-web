c382227b12989241a52dd8a5be5f958860f76ffe
Jan Mrázek <email@honzamrazek.cz>
2019-09-07 16:27:38 +0200

    Strip CSS

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+    // Specify the paths to all of the template files in your project
+    content: [
+        'App/**/*.latte'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    whitelist: ['tracy-debug-bar']
+})
+
+module.exports = {
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        purgecss,
+        require('cssnano')({
+            preset: 'default',
+        })
+    ]
+}
