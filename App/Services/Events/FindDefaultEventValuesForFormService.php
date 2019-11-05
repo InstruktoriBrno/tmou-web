21ee3c8c76cd580f752c8295dc52f55cbfb5bb1a
Jan Drábek <jan@drabek.cz>
2019-11-05 18:44:22 +0100

    Resolves #63: prefill typical dates when creating new event

--- App/Services/Events/FindDefaultEventValuesForFormService.php
+++ App/Services/Events/FindDefaultEventValuesForFormService.php
@@ -1,6 +1,8 @@
 <?php declare(strict_types=1);
 namespace InstruktoriBrno\TMOU\Services\Events;
 
+use DateTimeImmutable;
+
 class FindDefaultEventValuesForFormService
 {
     /** @var FindLatestEventService */
@@ -21,8 +23,16 @@ class FindDefaultEventValuesForFormService
         return [
             'number' => $latestEvent !== null ? $latestEvent->getNumber() + 1 : 1,
             'hasQualification' => true,
+            'qualificationStart' => (new DateTimeImmutable('last saturday of September'))->setTime(18, 0, 0),
+            'qualificationEnd' => (new DateTimeImmutable('last saturday of September'))->setTime(22, 0, 0),
+            'registrationDeadline' => (new DateTimeImmutable('last saturday of September'))->setTime(22, 0, 0),
+            'eventStart' => (new DateTimeImmutable('first friday of November'))->setTime(17, 0, 0),
+            'eventEnd' => (new DateTimeImmutable('first friday of November'))->modify('+37 hours'),
+            'changeDeadline' => (new DateTimeImmutable('first friday of November'))->modify('-5 days')->setTime(23, 59, 59),
             'qualifiedTeamCount' => 225,
             'totalTeamCount' => 250,
+            'amount' => 800,
+            'paymentDeadline' => (new DateTimeImmutable('last day of October')),
         ];
     }
 }
