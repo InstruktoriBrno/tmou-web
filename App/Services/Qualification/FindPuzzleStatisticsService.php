244c18677e1330f98e9dac8c83fef36ef99e5171
Jan Drábek <jan@drabek.cz>
2023-07-27 09:40:39 +0200

    Small fixes 2

--- App/Services/Qualification/FindPuzzleStatisticsService.php
+++ App/Services/Qualification/FindPuzzleStatisticsService.php
@@ -27,21 +27,32 @@ class FindPuzzleStatisticsService
         return $this->entityManager->getConnection()->executeQuery( // @phpstan-ignore-line
             <<<SQL
                 SELECT
-                puzzle.name AS puzzle_name,
-                COUNT(answer.id) AS solved_team_count,
-                SEC_TO_TIME(MIN(TIMESTAMPDIFF(SECOND, event.qualification_start, answer.answered_at))) AS fastest_answer_time,
-                team.name AS fastest_team_name
+                    puzzle.name AS puzzle_name,
+                    COUNT(answer.id) AS solved_team_count,
+                    SEC_TO_TIME(MIN(TIMESTAMPDIFF(SECOND, event.qualification_start, answer.answered_at))) AS fastest_answer_time,
+                    t.team_name AS fastest_team_name
                 FROM puzzle
                 JOIN level ON level.id = puzzle.level_id
                 JOIN event oN event.id = level.event_id
                 LEFT JOIN answer ON answer.puzzle_id = puzzle.id AND answer.correct = TRUE
-                LEFT JOIN team ON answer.team_id = team.id
+                LEFT JOIN (
+                    SELECT
+                        answer.puzzle_id,
+                        team.id as team_id,
+                        team.name as team_name,
+                        TIMESTAMPDIFF(SECOND, event.qualification_start, answer.answered_at),
+                        ROW_NUMBER() OVER (PARTITION BY answer.puzzle_id ORDER BY TIMESTAMPDIFF(SECOND, event.qualification_start, answer.answered_at) ASC) AS position
+                    FROM answer
+                    JOIN team ON answer.team_id = team.id
+                    JOIN event on event.id = team.event_id AND event.id = ?
+                    WHERE answer.correct = TRUE
+                ) t ON t.puzzle_id = answer.puzzle_id AND position = 1
                 WHERE level.event_id = ?
                 GROUP BY puzzle.id
                 ORDER BY puzzle.id
             SQL,
-            [$event->getId()],
-            [ParameterType::INTEGER]
+            [$event->getId(), $event->getId()],
+            [ParameterType::INTEGER, ParameterType::INTEGER]
         )->fetchAllAssociative();
     }
 }
